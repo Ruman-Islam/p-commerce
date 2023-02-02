ca96d6761a5c69ef7f652bb3077b68b546f4358f
Ruman-Islam <rumanislam48@gmail.com>
2023-02-02 13:05:47 +0600

    shopping cart done

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,16 +1,38 @@
 import Layout from "@/components/Layout";
 import data from "@/utils/data";
+import { Store } from "@/utils/Store";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useContext } from "react";
 
 const ProductScreen = () => {
+  const { state, dispatch } = useContext(Store);
+  const router = useRouter();
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
+
   if (!product) {
     return <div>Product Not Found</div>;
   }
+
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+
+    if (product.countInStock < quantity) {
+      alert("Sorry. Product is out of stock");
+      return;
+    }
+
+    dispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...product, quantity: quantity },
+    });
+    router.push("/cart");
+  };
+
   return (
     <Layout title={product.name}>
       <div className="py-2">
@@ -49,7 +71,12 @@ const ProductScreen = () => {
               <div>Status</div>
               <div>{product.countInStock > 0 ? "In stock" : "Unavailable"}</div>
             </div>
-            <button className="primary-button w-full">Add to cart</button>
+            <button
+              className="primary-button w-full"
+              onClick={() => addToCartHandler()}
+            >
+              Add to cart
+            </button>
           </div>
         </div>
       </div>
